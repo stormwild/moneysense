58726aa797601fcd65cb4cbf38f601a23d24b300
Alexander Torrijos <artorrijos@gmail.com>
2018-02-19 22:43:10 +0800

    Initial commit

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,12 +5,12 @@
 
   @if (!have_posts())
     <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+      {{  __('Sorry, no results were found.', 'sage') }}
     </div>
     {!! get_search_form(false) !!}
   @endif
 
-  @while(have_posts()) @php the_post() @endphp
+  @while(have_posts()) @php(the_post())
     @include('partials.content-search')
   @endwhile
 
