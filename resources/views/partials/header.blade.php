7c656ca3db28086691dbac7e80966115463d7222
Alexander Torrijos <artorrijos@gmail.com>
2018-03-27 21:40:54 +0800

    Added home icon and moved primary menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,15 @@
     @if (has_custom_logo())
         {!! the_custom_logo() !!}
     @endif
+    <div class="navbar-text">
+    <h3>Add goes here</h3>
+    <p>More text</p>
+    </div>
+  </nav>
+</header>
+<nav class="navbar navbar-expand-md navbar-dark bg-info">
+  <div class="container">
+    <a href="@php echo home_url() @endphp" class="navbar-brand"><i class="fa fa-home" aria-hidden="true"></i></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-primary" aria-controls="nav-primary" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,11 +22,7 @@
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container' => '', 'menu_class' => 'navbar-nav mr-auto', 'walker' => new App\NavWalker ]) !!}
       @endif
     </div>
-    <div class="navbar-text">
-    <h3>Add goes here</h3>
-    <p>More text</p>
-    </div>
-  </nav>
 
-</header>
+  </div>
+</nav>
 
